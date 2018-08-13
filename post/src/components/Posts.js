4b2eeaa315b21ee861a80c0672dc3dc39d2651d1
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-13 00:34:07 -0400

    Used mapStateToProps to connect state

--- post/src/components/Posts.js
+++ post/src/components/Posts.js
@@ -10,7 +10,7 @@ class Posts extends Component {
   }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title} </h3>
         <p> {post.body} </p>
@@ -25,4 +25,8 @@ class Posts extends Component {
   }
 }
 
-export default connect(null, { fetchPosts })(Posts);
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
