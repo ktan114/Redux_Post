cd1421a77f3522185b8c76310cf0132d360c4d2a
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-12 23:40:35 -0400

    Moved state to Post component and render post data

--- post/src/App.js
+++ post/src/App.js
@@ -1,23 +1,10 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
-import Posts from './components/Posts'
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
 
 class App extends Component {
-
-  componentWillMount() {
-    axios
-    .get("https://jsonplaceholder.typicode.com/posts")
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
   render() {
     return (
       <div className="App">
--- post/src/components/Posts.js
+++ post/src/components/Posts.js
@@ -1,12 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        console.log(res);
+        this.setState({ posts: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title} </h3>
+        <p> {post.body} </p>
+      </div>
+    ));
     return (
       <div>
-        <h1>Posts</h1>  
+        <h1>Posts</h1>
+        { postItems }
       </div>
-    )
+    );
   }
 }
 
