124d86f874a5e99bec3d7b22a8b34f942758d691
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-13 00:07:53 -0400

    Bring in createStore and applyMiddleware

--- post/src/App.js
+++ post/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
 
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
   render() {
     return (
-      <Provider>
+      <Provider store={store}>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
