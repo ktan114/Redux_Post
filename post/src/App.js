d86d457ec4aea2f73c625ac60f5fc0a40b613e71
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-12 23:31:42 -0400

    Imported axios

--- post/src/App.js
+++ post/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts'
 
 class App extends Component {
+
+  componentWillMount() {
+    console.log(123)
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,9 +18,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Posts />
       </div>
     );
